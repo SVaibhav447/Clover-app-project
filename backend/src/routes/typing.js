48196cd74bf2c952e046b1870ec8a96d97fc9727
VAIBHAV SHARMA <sharmavaibhav447@gmail.com>
2025-10-04 14:20:50 +0530

    Refactor typing route: improve error handling and response messages

--- backend/src/routes/typing.js
+++ backend/src/routes/typing.js
@@ -2,20 +2,32 @@ const store = require('../store');
 const Room = require('../models/Room');
 
 module.exports = async (req, res, next) => {
-  const roomObj = req.fields.room;
-  if (!roomObj) return res.status(400).send('room id required');
+  try {
+    const roomObj = req.fields.room;
+    if (!roomObj) return res.status(400).send('room id required');
 
-  const roomID = roomObj._id;
-  const isTyping = req.fields.isTyping;
+    const roomID = roomObj._id;
+    const isTyping = req.fields.isTyping;
 
-  if (!roomID) res.status(400).send('room id required');
+    if (!roomID) return res.status(400).send('room id required');
 
-  const room = await Room.findById(roomID);
+    const room = await Room.findById(roomID);
 
-  room.people.forEach((person) => {
-    if (person.toString() !== req.user.id.toString())
-      store.io.to(person.toString()).emit('typing', { id: req.user.id, roomID, isTyping });
-  });
+    if (!room) return res.status(404).send('Room not found');
 
-  res.status(200).send('ok');
+    room.people.forEach((person) => {
+      if (person.toString() !== req.user.id.toString()) {
+        store.io.to(person.toString()).emit('typing', {
+          id: req.user.id,
+          roomID,
+          isTyping,
+        });
+      }
+    });
+
+    res.status(200).send('ok');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
 };
